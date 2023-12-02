8e24439244437a3fbf84306fbf18f2e4f9c0eae7
Sirisha <138793236+Sirisha462@users.noreply.github.com>
2023-12-02 15:53:09 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -5,28 +5,26 @@ const { defineConfig, devices } = require('@playwright/test');
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// require('dotenv').config(); 
-
+// require('dotenv').config();
 
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
 module.exports = defineConfig({
-  timeout: 19 * 60 * 1000,
+  timeout: 10 * 60 * 1000,
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: false,
+  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html'],
-  ['allure-playwright', { outputFolder: 'my-allure-results' }]
-  ],
+  // reporter: 'html',
+  reporter: [['./myReporter.js'],['html']],
+  
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
