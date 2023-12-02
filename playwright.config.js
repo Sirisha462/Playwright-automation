1d126cd70744dfdacfe3444249d43df1f0d97ade
Sirisha <138793236+Sirisha462@users.noreply.github.com>
2023-12-02 15:55:31 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,7 @@ module.exports = defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   // reporter: 'html',
-  reporter: [['./myReporter.js'],['html']],
+  reporter: [['html']],
   
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
